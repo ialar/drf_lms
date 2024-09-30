ef42ccf972fc86dc867d53880521d5989b81312e
Igor Larionov <jedialar@gmail.com>
2024-09-30 16:22:09 +0300

    add filters for Payment

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import Payment
+from users.serializers import PaymentSerializer
+
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'method',)
+    ordering_fields = ('date',)
+    search_fields = ('amount', 'method',)
