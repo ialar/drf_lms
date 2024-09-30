0d766d817ba303822ee608657e34c0c8a02a1b14
Igor Larionov <jedialar@gmail.com>
2024-09-30 16:20:04 +0300

    update settings.py and urls.py

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "users",
     "materials",
 ]
@@ -74,6 +75,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
