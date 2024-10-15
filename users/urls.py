c38e6363aa0481803750d60980c551bc49c3c576
Igor Larionov <jedialar@gmail.com>
2024-10-15 22:16:18 +0300

    add payment controllers and urls

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from rest_framework.permissions import AllowAny
-from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet, UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
-    UserDeleteAPIView
+from users.views import UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDeleteAPIView, PaymentCreateAPIView, PaymentListAPIView
 
 app_name = UsersConfig.name
-
-router = SimpleRouter()
-router.register(r"payment", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
@@ -20,6 +16,6 @@ urlpatterns = [
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
     path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
+    path("payment/create/", PaymentCreateAPIView.as_view(), name="create-payment"),
+    path("payments/", PaymentListAPIView.as_view(permission_classes=(AllowAny,)), name="payments"),
 ]
-
-urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -2,21 +2,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
 from users.permissions import IsAdmin, IsActualUser
 from users.serializers import PaymentSerializer, UserSerializer, UserSerializerReadOnly
-
-
-class PaymentViewSet(ModelViewSet):
-    queryset = Payment.objects.all()
-    serializer_class = PaymentSerializer
-
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter, ]
-    filterset_fields = ("paid_course", "paid_lesson", "method")
-    ordering_fields = ("date",)
-    search_fields = ("amount", "method")
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
 
 
 class UserCreateAPIView(CreateAPIView):
@@ -51,3 +41,26 @@ class UserUpdateAPIView(UpdateAPIView):
 class UserDeleteAPIView(DestroyAPIView):
     queryset = User.objects.all()
     permission_classes = (IsAuthenticated, IsAdmin)
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request.user)
+        product_id = create_stripe_product(payment)
+        price_id = create_stripe_price(product_id, payment)
+        session_id, session_url = create_stripe_session(price_id)
+        payment.session_id = session_id
+        payment.link = session_url
+        payment.save()
+
+
+class PaymentListAPIView(ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ("paid_course", "paid_lesson", "method", "amount", "date")
+    ordering_fields = ("date",)
+    search_fields = ("amount", "method", "session_id")
